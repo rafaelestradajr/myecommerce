67efbf6279245ed017e82cdac5d75c4108db0129
Rafael Estrada <rafaelestrada@comcast.net>
2021-03-25 21:56:51 -0700

    update header

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Home=()=>(
+  
+    <div>
+      <p>react home</p>
+    </div>
+  );
+
+
+export default Home;
---
+++ src/pages/auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Login=()=>{
+    return(
+        <div>
+            <p>n</p>
+        </div>
+    );
+};
+
+export default Login;
