36f0d534c91db06a848d6fd4c76a051ee7bc698f
Rafael Estrada <rafaelestrada@comcast.net>
2021-03-26 20:48:02 -0700

    created links for nav bar

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,45 +1,42 @@
-import React,{useState} from 'react';
-import {Menu} from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined, UserAddOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+import {Link} from 'react-router-dom';
 
-const { SubMenu,Item } = Menu;//Menu.Submenu
+const { SubMenu, Item } = Menu; //Menu.Submenu
 
-const Header =()=>{
-    const [current,setCurrent] = useState('home')
-    const handleClick = (e)  => {
-        console.log(e.key)
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+  const handleClick = (e) => {
+    console.log(e.key);
 
-
-setCurrent(e.key);
-    };
-return(
-
-
+    setCurrent(e.key);
+  };
+  return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <Item key="home" icon={<MailOutlined />}>
-      Home
-        </Item>
+      <Item key="home" icon={<AppstoreOutlined /> }>
+       <Link to='/'>Home</Link>
+      </Item>
 
-        <Item key="register" icon={<UserAddOutlined />} className='float-right'>
-    Register
-        </Item>
+      <Item key="register" icon={<UserAddOutlined />} className="float-right">
+      <Link to='/register'>Register</Link>
+      </Item>
 
-        <Item key="login" icon={<UserAddOutlined />} className='float-right'>
-           Login
-        </Item>
+      <Item key="login" icon={<UserAddOutlined />} className="float-right">
+      <Link to='/login'>Login</Link>
+      </Item>
 
-        
-        <SubMenu  icon={<SettingOutlined />} title="Username">
-         
-      
-            <Item key="setting:1">Option 1</Item>
-            <Item key="setting:2">Option 2</Item>
-         
-          
-        </SubMenu>
-        
-      </Menu>
-);
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+      </SubMenu>
+    </Menu>
+  );
 };
 
 export default Header;
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Login=()=>{
     return(
         <div>
-            <p>n</p>
+            <p>Login</p>
         </div>
     );
 };
